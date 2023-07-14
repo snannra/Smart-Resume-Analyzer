29e404176e4564e93d84e6f76b94859051963a83
Sohan Nannra <49798755+snannra@users.noreply.github.com>
2023-07-13 22:26:37 -0700

    commit

--- Smart_Resume_Analyzer.py
+++ Smart_Resume_Analyzer.py
@@ -16,3 +16,79 @@ from Courses import ds_courses, web_course, android_course, ios_course, uiux_cou
 import pafy
 import plotly.express as px
 import youtube_dl
+
+connection = pymysql.connect(host='localhost',user='root',password='Bluefire49!@',db='sra')
+cursor = connection.cursor()
+
+st.set_page_config(
+    page_title="Smart Resume Analyzer",
+    page_icon='/Logo/SRA_Logo.ico',
+)
+
+def run():
+    st.title("Smart Resume Analyzer")
+    st.sidebar.markdown("# Choose User")
+    activities = ["Normal User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    img = Image.open('/Logo/SRA_Logo.jpg')
+    img = img.resize((250,250))
+    st.image(img)
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IS NOT EXISTS SRA;"""
+    cursor.execute(db_sql)
+
+    # Create table
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                    Name varchar(100) NOT NULL,
+                    Email_ID CARCHAR(50) NOT NULL,
+                    resume_score VARCHAR(8) NOT NULL,
+                    Timestamp VARCHAR(50) NOT NULL,
+                    Page_no VARCHAR(5) NOT NULL,
+                    Predicted_Field VARCHAR(25) NOT NULL,
+                    User_level VARCHAR(30) NOT NULL,
+                    Actual_skills VARCHAR(300) NOT NULL,
+                    Recommended_skills VARCHAR(300) NOT NULL,
+                    Recommended_courses VARCHAR(600) NOT NULL,
+                    PRIMARY KEY (ID));
+                    """
+    
+    cursor.execute(table_sql)
+    if choice == 'Normal User':
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            save_image_path = './Uploaded_Resumes/' + pdf_file_name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                resume_text = pdf_reader(save_image_path)
+                st.header("**Resume Analysis**")
+                st.success("Hello " + resume_data['name'])
+                st.subheader("**Your Basic Info**")
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+    
+    converter.close()
+    fake_file_handle.close()
+    return text
